76546e38b8bee471084a85dd44cd6cc333dfb001
Will Valentine <will.valentine1603@gmail.com>
2022-05-26 11:10:59 +0100

    gf

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,6 +2,6 @@ class List < ApplicationRecord
   has_many :bookmarks
   has_many :movies, through: :bookmarks, dependent: :destroy
 
-  validates :name, presence: true
+  validates :name
   validates :name, uniqueness: true
 end
